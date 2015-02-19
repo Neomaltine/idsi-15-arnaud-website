a6a9a5fe72fd7fa9c1ad71a2e6bf2293995b702c
Neomaltine <neomaltine@me.com>
2015-02-19 17:35:00 +0100

    TEst 2

--- testmaven/src/testmaven/main.java
+++ testmaven/src/testmaven/main.java
@@ -14,7 +14,7 @@ public class main {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World");
-		System.out.println("Hello World");
+		System.out.println("Hello World 2");
 		
 	}
 
