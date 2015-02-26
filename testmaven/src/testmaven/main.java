777f8ff69e9d26e515896ed15a0f20c73eabcec3
Neomaltine <neomaltine@me.com>
2015-02-26 14:57:14 +0100

    add line of code "test123"

--- testmaven/src/testmaven/main.java
+++ testmaven/src/testmaven/main.java
@@ -16,6 +16,8 @@ public class main {
 		System.out.println("Hello World");
 		System.out.println("Hello World 2");
 		System.out.println("Test123");
+		System.out.println("Test123");
+		System.out.println("Test123");
 		
 	}
 
